00b823099fd56e068a4928dce8a3d1df09f02ded
welhan <78246470+Welhan@users.noreply.github.com>
2025-09-07 10:31:15 +0700

    adding base_url to constants

--- configs/constants.js
+++ configs/constants.js
@@ -1,11 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const constants = {
   transactionPath: "logs/transactions",
   errorPath: "logs/errors",
   websiteName: "Tutor Website",
-  developmentNotes: [
-    "Bug Fixing Upload and Level Up (v1.0.1)",
-    "Bug Fixing Worker Untuk Update Level User (v1.0.1)",
-    "Change Luckyspin Coupon Expired Date From 3 days to 7 days (v1.0.1)",
-  ],
+  developmentNotes: {
+    "v 1.0.0": "Initial release",
+  },
+  base_url: `http://localhost:${process.env.APP_PORT}`,
 };
 module.exports = { constants };
