4ad3bd33fa21370eedc381af00e2da8de7a6454f
welhan <78246470+Welhan@users.noreply.github.com>
2025-09-07 10:16:48 +0700

    prepare and setting db.js

--- configs/db.js
+++ configs/db.js
@@ -2,6 +2,8 @@ const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const isProduction = process.env.NODE_APP === "production";
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -9,7 +11,7 @@ const sequelize = new Sequelize(
   {
     host: "localhost",
     dialect: "mysql",
-    logging: false,
+    logging: isProduction ? false : console.log,
     port: process.env.DB_PORT ? process.env.DB_PORT : 3306,
   }
 );
